3c20e4fe1fcfb5ec45471b91eea76c1313a01287
Perry Nguyen <pfnguyen@hanhuy.com>
2017-11-11 17:11:41 -0800

    More actives parsing placeholders

--- shared/src/main/scala/actives.scala
+++ shared/src/main/scala/actives.scala
@@ -78,25 +78,52 @@ object DebuffEffect {
 
 
 object ActiveEffects {
+  // 1st arg, targeting flag, 0 = self, 1 = ST, 2 = AOE, 3 = random
+  // 2nd arg, target type, 1 = enemy, 2 = allies, 3 = self, 5 = allies no-self, 6 = ko'd, enemy-reaper
+
+  // repeated count = range min-max
   def apply(x: Int, y: Int, z: Int, xs: List[Int]): ActiveEffect = (x, y, z) match {
     case (aoeflag, 1, 1) => PhysicalEffect.decode(aoeflag == 2, xs)
+    case (aoeflag, 2, 2) => HealEffect.decode(aoeflag == 2, xs)
     case (aoeflag, 6, 2) => HealEffect.decode(aoeflag == 2, xs)
-    case (_, 6, 4) => UnknownActiveEffect // raise effect, args pct heal
+    case (_, 2, 3) => UnknownActiveEffect // BuffEffect.decode(tgt, xs)
     case (_, 3, 3) => UnknownActiveEffect // BuffEffect.decode(tgt, xs)
+    case (_, 6, 4) => UnknownActiveEffect // raise effect, args pct heal
     case (_, 2, 5) => UnknownActiveEffect // ally status ailments cure, args 1 2 3 4 5 6 7 8
+    case (0, 3, 5) => UnknownActiveEffect // self ailments cure, args 1 2 3 4 5 6 7 8 (or 0 for not cure)
     case (_, 1, 6) => UnknownActiveEffect // StatusAilmentEffect
     case (aoeflag, 2, 8) => HealEffect.decode(aoeflag == 2, xs)
+    case (_, 1, 9) => UnknownActiveEffect // pcthp damage, pct pct 100?
+    case (_, 1, 10) => UnknownActiveEffect // physical osmose drainratio ratio 100?
+    case (_, 1, 13) => UnknownActiveEffect // delay damage, args: delay 0? 0? 2? ??? ratio
     case (aoeflag, 1, 15) => MagicalEffect.decode(aoeflag == 2, xs)
     case (_, 3, 17) => UnknownActiveEffect // mp heal, self? args N
+    case (0, 3, 20) => UnknownActiveEffect // store +ratio maxratio 0?
     case (aoeflag, 1, 21) => PhysicalEffect.decode(aoeflag == 2, xs)
+    case (_, 1, 23) => UnknownActiveEffect // magic damage, bonus to type, args: type 0? 0? bonus-ratio
     case (aoeflag, 1, 24) => DebuffEffect.decode(aoeflag == 2, xs)
+    case (_, 1, 25) => UnknownActiveEffect // physical drain drainratio ratio 100?
     case (_, 2, 27) => UnknownActiveEffect // reraise, args pct, turns
+    case (_, 2, 28) => UnknownActiveEffect // salve, args = valid item ids
     case (_, 2, 30) => UnknownActiveEffect // MP regen, args: scale, 1?, base, turns
     case (_, 3, 30) => UnknownActiveEffect // MP regen
+    case (1, 5, 31) => UnknownActiveEffect // entrust no args ("none")
     case (aoeflag, 1, 33) => ImperilEffect.decode(aoeflag == 2, xs)
+    case (_, 1, 35) => UnknownActiveEffect // Instance KO arg chance
     case (_, 1, 37) => UnknownActiveEffect // steal, arg bonus chance
+    case (_, 2, 38) => UnknownActiveEffect // heal chance, 7 args, last string, 0? 0? chance% heal-amount heal-ratio 5000040? text
     case (aoeflag, 1, 40) => HybridEffect.decode(aoeflag == 2, xs)
+    case (_, 1, 41) => UnknownActiveEffect // Fixed damage arg: amount
+    case (3, 1, 42) => UnknownActiveEffect // repeated autoattacks args: 0, 0, count-min, count-max, ratio-each
+    case (_, 1, 43) => UnknownActiveEffect // crit attack args 0 0 ratio miss-chance
+    case (0, 3, 44) => UnknownActiveEffect // dual-black
+    case (0, 3, 45) => UnknownActiveEffect // dual cast
     case (_, 1, 47) => UnknownActiveEffect // libra effect arg 134
+    case (0, 1, 50) => UnknownActiveEffect // Throw
+    case (0, 3, 48) => UnknownActiveEffect // Drink
+    case (0, 3, 51) => UnknownActiveEffect // ignorefatal hpthresh chance 0? n-times
+    case (1, 1, 52) => UnknownActiveEffect // Jump 0? 0? delay delay ratio
+    case (0, 3, 53) => UnknownActiveEffect // hide args min-max
     case (_, 3, 54) => UnknownActiveEffect // dodge physical args: count, turns
     case (_, 2, 56) => UnknownActiveEffect // HealEffect, singing, args: ratio, 1?, base, turns
     case (_, 3, 56) => UnknownActiveEffect // Skip turns, args: _, 1?, _, turns
@@ -104,9 +131,14 @@ object ActiveEffects {
     case (_, 2, 58) => UnknownActiveEffect // buff effect, singing, 6 args, turns arg5
     case (_, 1, 59) => UnknownActiveEffect // dispel, "none" args
     case (_, 2, 59) => UnknownActiveEffect // ally dispel
+    case (0, 3, 61) => UnknownActiveEffect // provoke args: chance turns
+    case (0, 3, 64) => UnknownActiveEffect // self-restore pcthp pctmp
     case (aoeflag, 1, 70) => MagicalEffect.decode(aoeflag == 2, xs)
     case (_, 1, 76) => UnknownActiveEffect // steal gil, args low/max pct
+    case (_, 1, 81) => UnknownActiveEffect // sacrifice hp -> physical , args 0? 0? 0? 0? 0? 0? ratio pcthp 2000315? 1?
+    case (1, 3, 84) => UnknownActiveEffect // sealing blade args 1 0 1 (turns, spells?)
     case (aoeflag, 1, 88) => StopEffect.decode(aoeflag == 2, xs)
+    case (0, 3, 101) => UnknownActiveEffect // damage reduction, args pct, turns, 1?
     case (_, 1, 103) => UnknownActiveEffect // spr scaling magic attack, args, spr-mag ratio, max, ratio
     case (_, 1, 105) => UnknownActiveEffect // mp scaling magic attack, args, mp-mag ratio, max, ratio
     case (_, 2, 111) => UnknownActiveEffect // remove debuff from ally, 6 args
